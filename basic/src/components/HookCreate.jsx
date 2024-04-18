4749ce62a28cc61570f9ca968263190e62a16f76
KGM <KGM@CASL6-37-165.kgisledu.com>
2024-04-18 12:58:28 +0530

    custom hook

--- basic/src/components/HookCreate.jsx
+++ basic/src/components/HookCreate.jsx
@@ -8,7 +8,9 @@ export default function HookCreate() {
     <div>
       <input type="number" value={val1} onChange={onChange1} />
       <input type="number" value={val2} onChange={onChange2} />
-      <button onClick={add}>Add</button>
+      <button onClick={add}>
+        Add <i class="fa-solid fa-plus"></i>
+      </button>
       {result !== null && <p>Result: {result}</p>}
     </div>
   );
